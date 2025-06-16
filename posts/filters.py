67ec60f8bba83e61bd174e358eb0666b78e9978a
nikacahduneli3 <nbaidoshvili12@gmail.com>
2025-06-16 19:00:47 +0400

    blabla

--- posts/filters.py
+++ posts/filters.py
@@ -3,13 +3,10 @@ from .models import  Post
 
 
 class PostFilterSet(django_filters.FilterSet):
-    title = django_filters.CharFilter(
-        lookup_expr='icontains'
-    )
+    title = django_filters.CharFilter(lookup_expr='icontains')
     publish_date = django_filters.DateFilter()
-    content = django_filters.CharFilter(
-        lookup_expr='icontains'
-    )
+    content = django_filters.CharFilter(lookup_expr='icontains')
+
 
     class Meta:
         model = Post
